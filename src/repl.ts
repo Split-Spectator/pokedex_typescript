553e67377a65528672d231d87bc87ab810b00152
Split-Spectator <aosc9vma@mailer.me>
2025-10-09 16:39:09 -0400

    startREPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,19 @@
+import { createInterface } from "readline";
+
 export function cleanInput(input: string): string[] {
   return input.trim().toLowerCase().split(/\s+/).filter(word => word !== '');
   }
-  
+
+  export function startREPL(): void {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > "
+    });
+    rl.prompt();
+    rl.on("line", async (input) => {
+        const words = cleanInput(input);
+        if (words.length > 0) console.log(`Your command was: ${words[0]}`);
+        rl.prompt();
+    })
+  }

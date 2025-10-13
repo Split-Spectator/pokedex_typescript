38e51878d1edaf9b23f6c2348a33cf7604242339
Split-Spectator <aosc9vma@mailer.me>
2025-10-13 12:17:43 -0400

    help & exit

--- src/repl.ts
+++ src/repl.ts
@@ -1,19 +1,50 @@
 import { createInterface } from "readline";
+import { getCommands } from "./commands.js";
+
 
 export function cleanInput(input: string): string[] {
-  return input.trim().toLowerCase().split(/\s+/).filter(word => word !== '');
+    return input
+      .toLowerCase()
+      .trim()
+      .split(" ")
+      .filter((word) => word !== "");
   }
+  
 
-  export function startREPL(): void {
-    const rl = createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: "Pokedex > "
-    });
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "pokedex > ",
+  });
+
+  rl.prompt();
+
+  rl.on("line", async (input) => {
+    const words = cleanInput(input);
+    if (words.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    const commandName = words[0];
+
+    const commands = getCommands();
+    const cmd = commands[commandName];
+    if (!cmd) {
+      console.log(
+        `Unknown command: "${commandName}". Type "help" for a list of commands.`,
+      );
+      rl.prompt();
+      return;
+    }
+
+    try {
+      cmd.callback(commands);
+    } catch (err) {
+      console.log(err);
+    }
+
     rl.prompt();
-    rl.on("line", async (input) => {
-        const words = cleanInput(input);
-        if (words.length > 0) console.log(`Your command was: ${words[0]}`);
-        rl.prompt();
-    })
-  }
+  });
+}
